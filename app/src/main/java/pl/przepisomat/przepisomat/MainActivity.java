f8adce9ef41263bcf4ea9d01ddf64b2e11e0777d
Michal-l\Majkel <mih.pago@op.pl>
2018-05-21 22:08:39 +0200

    Dokończenie listy wyszukiwania. Dodanie progressbara oraz zabezpieczenie przed wpisywanie zanim pobiorą się wyniki

--- app/src/main/java/pl/przepisomat/przepisomat/MainActivity.java
+++ app/src/main/java/pl/przepisomat/przepisomat/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setDefaults();
+        setDefaults(false);
     }
 
     @Override
--- app/src/main/java/pl/przepisomat/przepisomat/SearchActivity.java
+++ app/src/main/java/pl/przepisomat/przepisomat/SearchActivity.java
@@ -1,5 +1,7 @@
 package pl.przepisomat.przepisomat;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,12 +9,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.gson.Gson;
 
@@ -29,29 +36,47 @@ public class SearchActivity extends BaseActivity {
 
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> arrayList;
+    private ArrayList<String> idArrayList;
     private ListView listView;
+    private SearchView searchView;
+    private boolean isDataLoaded = false;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        setDefaults();
+        setDefaults(true);
 
         this.arrayList = new ArrayList<String>();
+        this.idArrayList = new ArrayList<String>();
+
+        this.progressBar = findViewById(R.id.progressbar);
+        this.progressBar.setVisibility(View.VISIBLE);
 
         Call<RecipesNames> recipesNamesCall = ApiService.getService().getRecipesNames();
         recipesNamesCall.enqueue(new Callback<RecipesNames>() {
             @Override
             public void onResponse(@NonNull Call<RecipesNames> call, @NonNull Response<RecipesNames> response) {
                 RecipesNames recipesNames = response.body();
-
+                SearchActivity.this.isDataLoaded = true;
+                invalidateOptionsMenu();
+                SearchActivity.this.progressBar.setVisibility(View.INVISIBLE);
                 for(RecipeName recipeName : recipesNames.names )
                 {
                     SearchActivity.this.arrayList.add(recipeName.name);
+                    SearchActivity.this.idArrayList.add(String.valueOf(recipeName.id));
                 }
                 SearchActivity.this.arrayAdapter = new ArrayAdapter<String>(SearchActivity.this, android.R.layout.simple_list_item_1, SearchActivity.this.arrayList);
                 SearchActivity.this.listView = findViewById(R.id.searchListView);
                 SearchActivity.this.listView.setAdapter(SearchActivity.this.arrayAdapter);
+                SearchActivity.this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Log.d("TAG", SearchActivity.this.arrayList.get(i).toString());
+                        Log.d("TAG", SearchActivity.this.idArrayList.get(i).toString());
+                    }
+                });
             }
 
             @Override
@@ -63,38 +88,43 @@ public class SearchActivity extends BaseActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search_view_menu, menu);
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-        searchItem.expandActionView();
-        SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                SearchActivity.this.arrayAdapter.getFilter().filter(query);
-                return false;
-            }
+        if(SearchActivity.this.isDataLoaded == true) {
+            getMenuInflater().inflate(R.menu.search_view_menu, menu);
+            MenuItem searchItem = menu.findItem(R.id.action_search);
+            searchItem.expandActionView();
+            this.searchView = (SearchView) searchItem.getActionView();
+            this.searchView.setEnabled(false);
+            this.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    if (SearchActivity.this.arrayAdapter != null) {
+                        SearchActivity.this.arrayAdapter.getFilter().filter(query);
+                    }
+                    return false;
+                }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                SearchActivity.this.arrayAdapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-        searchView.setQueryHint("Wyszukaj przepisz...");
-        searchView.setIconified(false);
-        int searchPlateId = searchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
-        View searchPlate = searchView.findViewById(searchPlateId);
-        if (searchPlate!=null) {
-            searchPlate.setBackgroundColor(getResources().getColor(R.color.colorMenu));
-            int searchTextId = searchPlate.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
-            TextView searchText = (TextView) searchPlate.findViewById(searchTextId);
-            if (searchText!=null) {
-                searchText.setTextColor(Color.BLACK);
-                searchText.setHintTextColor(Color.BLACK);
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    if (SearchActivity.this.arrayAdapter != null) {
+                        SearchActivity.this.arrayAdapter.getFilter().filter(newText);
+                    }
+                    return false;
+                }
+            });
+            this.searchView.setQueryHint("Wyszukaj przepisz...");
+            this.searchView.setIconified(false);
+            int searchPlateId = this.searchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
+            View searchPlate = this.searchView.findViewById(searchPlateId);
+            if (searchPlate != null) {
+                searchPlate.setBackgroundColor(getResources().getColor(R.color.colorMenu));
+                int searchTextId = searchPlate.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+                TextView searchText = (TextView) searchPlate.findViewById(searchTextId);
+                if (searchText != null) {
+                    searchText.setTextColor(Color.BLACK);
+                    searchText.setHintTextColor(Color.BLACK);
+                }
             }
         }
-
-
         return super.onCreateOptionsMenu(menu);
     }
 }
